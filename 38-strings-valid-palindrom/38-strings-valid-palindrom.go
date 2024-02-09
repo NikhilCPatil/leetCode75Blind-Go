e23b9a0fc04e5c8b21d46297d7027efdf83410d4
Nikhil Patil <nikhil.patil@contractoralefeducation.com>
2024-02-10 00:12:16 +0530

    Updated condition

--- 38-strings-valid-palindrom/38-strings-valid-palindrom.go
+++ 38-strings-valid-palindrom/38-strings-valid-palindrom.go
@@ -6,9 +6,7 @@ import (
 )
 
 func validPalandrom(s string)bool{
-	fmt.Println(s)
 	s = preProcess(s)
-fmt.Println(s)
 	left := 0
 	right := len(s)-1
 
@@ -24,29 +22,28 @@ fmt.Println(s)
 }
 
 func preProcess(s string)string{
-	var res strings.Builder
+	var result strings.Builder
 	for _, char := range s {
-		if isAlphaNumeric(char) {
-			res.WriteRune(toLower(char))
-		} 
+		if isAlphanumeric(char) {
+			result.WriteRune(toLower(char))
+		}
 	}
-
-	return res.String()
+	return result.String()
 }
 
 func toLower(char rune)rune{
-	if(char >= 'A' || char <= 'Z'){
+	if char >= 'A' && char <= 'Z' {
 		return char + 32
 	}
 	return char
 }
 
-func isAlphaNumeric(char rune)bool{
-return (char >= 'a' && char <= 'z') || (char >= 'A' && char <= 'Z') || (char >= '0' && char <= '9')
+func isAlphanumeric(char rune)bool{
+	return (char >= 'a' && char <= 'z') || (char >= 'A' && char <= 'Z') || (char >= '0' && char <= '9')
 }
 
 
 func main(){
-s := "A man, a plan, a canal: Panama"
-fmt.Println("Valid Palandrom",validPalandrom(s))
+	s := "A man, a plan, a canal: Panama"
+	fmt.Println("Valid Palandrom? ", validPalandrom(s))
 }
